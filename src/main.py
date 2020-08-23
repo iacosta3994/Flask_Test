6b7f5c2b74d34ebfc05d27c2253ef7d75af30e58
iacosta3994 <iacosta3994@gmail.com>
2020-08-23 09:47:33 -0700

    building function for decisions.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 
+    def decision(input_prompt, choice_a, choice_b, a_text, b_text):
+        choice = input(input_prompt)
+        if choice == choice_a:
+            print(a_text)
+        elif choice == choice_b:
+            print(b_text)
+        else:
+            print("That didn't seem to be one of the choices, try again.")
+            decision(input_prompt, choice_a, choice_b, a_text, b_text)
+
 
     StoryTime = True
 
@@ -12,13 +22,10 @@
         print (" A long, long, long time ago. There was an incredible hero that saved humanity. You ," + character_name + " , are not said hero." )
 
         ## Chapter One: The begining of the non-heroe heroe
+        input_prompt = "So tell me " + character_name + " are you feeling more spicey or cool?"
+        choice_a = 'spicey'
+        a_text = "Sweet! hmmm, I mean spicey.. Now you have spicey powers that will become mostly irrelevant until its neeeded to further the plot. hrmm I mean Adventure!!"
+        choice_b = 'cool'
+        b_text = "Thats cool, you can probably diffuse a high stakes situation with no effort."
 
-        choice = input("So tell me " + character_name + " are you feeling more spicey or cool?")
-        if choice == 'spicey':
-            print("Sweet! hmmm, I mean spicey.. Now you have spicey powers that will become mostly irrelevant until its neeeded to further the plot. hrmm I mean Adventure!!")
-            super_power = too_hot_to_handle = True
-        elif choice == 'cool':
-            print("Thats cool, you can probably diffuse a high stakes situation with no effort.")
-            super_power = cool_vibe = True
-        else:
-            print("Thats not spicey or cool, check your spelling next time")
+        decision(input_prompt,)
